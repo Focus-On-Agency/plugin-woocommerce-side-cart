7aa0c84fa97a95551cca7315891a5b2ec611c69e
pcatapan <pacho30000@gmail.com>
2026-06-01 10:30:28 +0200

    Prepare Composer package archive

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'focuson/woocommerce-side-cart',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5a8415acdef23392d9b2380d58d7317af294310e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'focuson/woocommerce-side-cart' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5a8415acdef23392d9b2380d58d7317af294310e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
